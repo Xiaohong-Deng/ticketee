aa029fd5049deae2ace575f12374917f3251d026
nicenick <worldsky86rough@gmail.com>
2017-08-21 19:00:38 +0800

    Change logic to correctly serve file from S3

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,7 +4,7 @@ class AttachmentsController < ApplicationController
   def show
     attachment = Attachment.find(params[:id])
     authorize attachment, :show?
-    send_file attachment.file.path, disposition: :inline
+    send_file file_to_send(attachment), disposition: :inline
     # recall we mount_uploader AttachmentUploader to :attachment.file
     # so attachment.file though is a string, is converted to an object
     # by CarrierWave and method path can be called on it to generate
@@ -18,4 +18,17 @@ class AttachmentsController < ApplicationController
     @ticket.attachments.build
     render layout: false
   end
+
+  private
+    def file_to_send(attachment)
+      if URI.parse(attachment.file.url).scheme
+        filename = "/tmp/#{attachment.attributes["file"]}"
+        File.open(filename, "wb+") do |tf|
+          td.write open(attachment.file.url).read
+        end
+        filename
+      else
+        attachment.file.path
+      end
+    end
 end
