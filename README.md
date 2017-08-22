8a80e22e13c1bc8ae87d6f6223c090a44ae3a7ef
nicenick <worldsky86rough@gmail.com>
2017-08-22 16:04:13 +0800

    Add Mailgun configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/Xiaohong-Deng/ticketee.png?branch=master)](https://travis-ci.org/Xiaohong-Deng/ticketee)
+
 ### About
 It's a project-ticket tracking system built in rails. It uses Travis CI and Heroku for deployment.
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.delivery_method = :smtp
+
+  host = "ticket-tracking-36097.herokuapp.com"
+
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         host,
+    authentication: :plain,
+  }
 end
