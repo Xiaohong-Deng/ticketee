15a70117a22b85673a332e687d0636fbcc984b6f
nicenick <worldsky86rough@gmail.com>
2017-09-01 23:04:53 +0800

    Create a middleware

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Ticketee
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # require "link_jumbler"
+    # config.middleware.use LinkJumbler, { "e" => "a" }
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
